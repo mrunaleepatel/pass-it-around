a8eb372d2dd8988a537441f55754a158316cca56
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-11 19:29:39 -0500

    test server

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express") 
+const app = express() 
+const PORT = process.env.PORT
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`)
+})
