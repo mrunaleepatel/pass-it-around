c89a65a28a7c9fa3ce4e0cdb9379a3f6df89ddbd
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-11 19:39:34 -0500

    homepage with /

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require("express")
 const app = express() 
 const PORT = process.env.PORT
 
-
+app.get('/', (req, res) => {
+    const bottles = 99;
+    res.send(`
+      <h1>${bottles} Bottles of mango juice on the wall</h1>
+      <a href="/${bottles - 1}">take one down and pass it around</a>
+    `);
+  });
 
 
 
