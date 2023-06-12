7a3bc1f8b20808d60d8127d4c788df3a994b7043
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-12 10:22:21 -0500

    if 0 bottles, take to homepage

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.get('/:number_of_bottles', (req, res) => {
     const bottles = Number(req.params.number_of_bottles);
     const nextBottles = bottles - 1;
 
+    const bottlesText = bottles > 0 ? `${bottles} Bottles of mango juice on the wall` : 'No more bottles of mango juice on the wall';
+    const Text = bottles > 0 ? `take one down and pass it around` : 'start over';
+    const Href = bottles > 0 ? `/${nextBottles}` : '/';
+
+    res.send(`
+      <h1>${bottlesText}</h1>
+      <a href="${Href}">${Text}</a>
+    `);
     
 })
 
